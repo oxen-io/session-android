b669739170a00ffeb07517b2c9c6f98938ee0916
jubb <hjubb@users.noreply.github.com>
2022-09-01 17:37:48 +1000

    refactor: title and VM changes

--- app/src/main/java/org/thoughtcrime/securesms/preferences/appearance/AppearanceSettingsActivity.kt
+++ app/src/main/java/org/thoughtcrime/securesms/preferences/appearance/AppearanceSettingsActivity.kt
@@ -2,19 +2,19 @@ package org.thoughtcrime.securesms.preferences.appearance
 
 import android.os.Bundle
 import android.os.PersistableBundle
+import androidx.activity.viewModels
 import dagger.hilt.android.AndroidEntryPoint
 import network.loki.messenger.R
 import org.thoughtcrime.securesms.PassphraseRequiredActionBarActivity
-import javax.inject.Inject
 
 @AndroidEntryPoint
 class AppearanceSettingsActivity: PassphraseRequiredActionBarActivity() {
 
-    @Inject
-    lateinit var viewModel: AppearanceSettingsViewModel
+    val viewModel: AppearanceSettingsViewModel by viewModels()
 
     override fun onCreate(savedInstanceState: Bundle?, persistentState: PersistableBundle?) {
         super.onCreate(savedInstanceState, persistentState)
         setContentView(R.layout.activity_appearance_settings)
+        supportActionBar!!.title = getString(R.string.activity_settings_message_appearance_button_title)
     }
 }
--- app/src/main/java/org/thoughtcrime/securesms/preferences/appearance/AppearanceSettingsViewModel.kt
+++ app/src/main/java/org/thoughtcrime/securesms/preferences/appearance/AppearanceSettingsViewModel.kt
@@ -1,12 +1,8 @@
 package org.thoughtcrime.securesms.preferences.appearance
 
 import androidx.lifecycle.ViewModel
-import dagger.hilt.android.lifecycle.HiltViewModel
-import org.session.libsession.database.StorageProtocol
-import javax.inject.Inject
 
-@HiltViewModel
-class AppearanceSettingsViewModel @Inject constructor(storage: StorageProtocol): ViewModel() {
+class AppearanceSettingsViewModel : ViewModel() {
 
 
 
