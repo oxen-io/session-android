7827fdf866c3183a649c0a791b73dffcabb68664
andrew <andrewgallasch@gmail.com>
2023-05-23 17:10:33 +0930

    Fix dialog

--- app/src/main/java/org/thoughtcrime/securesms/preferences/ListPreferenceDialog.kt
+++ app/src/main/java/org/thoughtcrime/securesms/preferences/ListPreferenceDialog.kt
@@ -1,5 +1,6 @@
 package org.thoughtcrime.securesms.preferences
 
+import android.content.Context
 import android.view.LayoutInflater
 import androidx.appcompat.app.AlertDialog
 import androidx.preference.ListPreference
@@ -7,36 +8,38 @@ import androidx.recyclerview.widget.DividerItemDecoration
 import network.loki.messenger.databinding.DialogListPreferenceBinding
 import org.thoughtcrime.securesms.conversation.v2.utilities.BaseDialog
 
-class ListPreferenceDialog(
-    private val listPreference: ListPreference,
-    private val dialogListener: () -> Unit
-) : BaseDialog() {
-    private lateinit var binding: DialogListPreferenceBinding
+fun listPreferenceDialog(
+    context: Context,
+    listPreference: ListPreference,
+    dialogListener: () -> Unit
+) : AlertDialog {
 
-    override fun setContentView(builder: AlertDialog.Builder) {
-        binding = DialogListPreferenceBinding.inflate(LayoutInflater.from(requireContext()))
-        binding.titleTextView.text = listPreference.dialogTitle
-        binding.messageTextView.text = listPreference.dialogMessage
-        binding.closeButton.setOnClickListener {
-            dismiss()
-        }
-        val options = listPreference.entryValues.zip(listPreference.entries) { value, title ->
-            RadioOption(value.toString(), title.toString())
-        }
-        val valueIndex = listPreference.findIndexOfValue(listPreference.value)
-        val optionAdapter = RadioOptionAdapter(valueIndex) {
-            listPreference.value = it.value
-            dismiss()
-            dialogListener.invoke()
-        }
-        binding.recyclerView.apply {
-            adapter = optionAdapter
-            addItemDecoration(DividerItemDecoration(requireContext(), DividerItemDecoration.VERTICAL))
-            setHasFixedSize(true)
-        }
-        optionAdapter.submitList(options)
-        builder.setView(binding.root)
-        builder.setCancelable(false)
+    val builder = AlertDialog.Builder(context)
+
+    val binding = DialogListPreferenceBinding.inflate(LayoutInflater.from(context))
+    binding.titleTextView.text = listPreference.dialogTitle
+    binding.messageTextView.text = listPreference.dialogMessage
+
+    val options = listPreference.entryValues.zip(listPreference.entries) { value, title ->
+        RadioOption(value.toString(), title.toString())
     }
+    val valueIndex = listPreference.findIndexOfValue(listPreference.value)
+    builder.setView(binding.root)
 
+    val dialog = builder.show()
+
+    val optionAdapter = RadioOptionAdapter(valueIndex) {
+        listPreference.value = it.value
+        dialog.dismiss()
+        dialogListener()
+    }.apply { submitList(options) }
+
+    binding.recyclerView.apply {
+        adapter = optionAdapter
+        addItemDecoration(DividerItemDecoration(context, DividerItemDecoration.VERTICAL))
+        setHasFixedSize(true)
+    }
+    binding.closeButton.setOnClickListener { dialog.dismiss() }
+
+    return dialog
 }
