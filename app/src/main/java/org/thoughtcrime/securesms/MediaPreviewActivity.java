1855e7e675f03112efdf48c8d6e5febaee3d7fad
0x330a <92654767+0x330a@users.noreply.github.com>
2023-10-04 10:59:11 +1100

    refactor: wrap in an if and log warning in the bad state

--- app/src/main/java/org/thoughtcrime/securesms/MediaPreviewActivity.java
+++ app/src/main/java/org/thoughtcrime/securesms/MediaPreviewActivity.java
@@ -533,11 +533,15 @@ public class MediaPreviewActivity extends PassphraseRequiredActionBarActivity im
 
       viewModel.setCursor(this, data.first, leftIsRecent);
 
-      int item = restartItem >= 0 ? restartItem : data.second >= 0 ? data.second : 0;
-      mediaPager.setCurrentItem(item);
+      if (restartItem >= 0 || data.second >= 0) {
+        int item = restartItem >= 0 ? restartItem : data.second;
+        mediaPager.setCurrentItem(item);
 
-      if (item == 0) {
-        viewPagerListener.onPageSelected(0);
+        if (item == 0) {
+          viewPagerListener.onPageSelected(0);
+        }
+      } else {
+        Log.w(TAG, "one of restartItem "+restartItem+" and data.second "+data.second+" would cause OOB exception");
       }
     }
   }
