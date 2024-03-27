fc0be3c9f39c6c04e4f380244adf9ae3787d983c
alansley <aclansley@gmail.com>
2024-03-27 13:08:42 +1100

    Fixed merge conflict

--- app/src/main/java/org/thoughtcrime/securesms/database/MmsSmsDatabase.java
+++ app/src/main/java/org/thoughtcrime/securesms/database/MmsSmsDatabase.java
@@ -173,24 +173,6 @@ public class MmsSmsDatabase extends Database {
     return identifiedMessages;
   }
 
-  public long getLastSentMessageFromSender(long threadId, String serializedAuthor) {
-    String order = MmsSmsColumns.NORMALIZED_DATE_SENT + " DESC";
-    String selection = MmsSmsColumns.THREAD_ID + " = " + threadId;
-
-    boolean isOwnNumber = Util.isOwnNumber(context, serializedAuthor);
-
-    // Try everything with resources so that they auto-close on end of scope
-    try (Cursor cursor = queryTables(PROJECTION, selection, order, null)) {
-      try (MmsSmsDatabase.Reader reader = readerFor(cursor)) {
-        MessageRecord messageRecord;
-        while ((messageRecord = reader.getNext()) != null) {
-          if (isOwnNumber && messageRecord.isOutgoing()) { return messageRecord.id; }
-        }
-      }
-    }
-    return -1;
-  }
-
   public MessageRecord getLastSentMessageRecordFromSender(long threadId, String serializedAuthor) {
     String order = MmsSmsColumns.NORMALIZED_DATE_SENT + " DESC";
     String selection = MmsSmsColumns.THREAD_ID + " = " + threadId;
