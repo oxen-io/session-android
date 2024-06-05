e95cc1c86ea1fa351d9aa917018cb3acf9608eaf
alansley <aclansley@gmail.com>
2024-06-05 15:11:10 +1000

    Finished updating libsession strings

--- libsession/src/main/java/org/session/libsession/utilities/ExpirationUtil.kt
+++ libsession/src/main/java/org/session/libsession/utilities/ExpirationUtil.kt
@@ -5,59 +5,92 @@ import org.session.libsession.R
 import java.util.concurrent.TimeUnit
 import kotlin.time.Duration
 import com.squareup.phrase.Phrase
+import kotlin.time.Duration.Companion.days
+import kotlin.time.Duration.Companion.hours
+import kotlin.time.Duration.Companion.minutes
 
 fun Context.getExpirationTypeDisplayValue(sent: Boolean) = if (sent) getString(R.string.disappearingMessagesSent) else getString(R.string.disappearingMessagesRead)
 
 object ExpirationUtil {
 
     // Keys for Phrase library substitution
-    const val HOURS_KEY = "hours"
+    const val DAYS_KEY    = "days"
+    const val HOURS_KEY   = "hours"
     const val MINUTES_KEY = "minutes"
     const val SECONDS_KEY = "seconds"
+    const val WEEKS_KEY   = "weeks"
 
     @JvmStatic
     fun getExpirationDisplayValue(context: Context, duration: Duration): String = getExpirationDisplayValue(context, duration.inWholeSeconds.toInt())
 
     @JvmStatic
-    fun getExpirationDisplayValue(context: Context, expirationTime: Int): String {
-        return if (expirationTime <= 0) {
+    fun getExpirationDisplayValue(context: Context, expirationTimeSecs: Int): String {
+        return if (expirationTimeSecs <= 0) {
             context.getString(R.string.off)
-        } else if (expirationTime < TimeUnit.MINUTES.toSeconds(1)) {
-            context.resources.getQuantityString(
-                R.plurals.expiration_seconds,
-                expirationTime,
-                expirationTime
-            )
-        } else if (expirationTime < TimeUnit.HOURS.toSeconds(1)) {
-            val minutes = expirationTime / TimeUnit.MINUTES.toSeconds(1).toInt()
-            context.resources.getQuantityString(R.plurals.expiration_minutes, minutes, minutes)
-        } else if (expirationTime < TimeUnit.DAYS.toSeconds(1)) {
-            val hours = expirationTime / TimeUnit.HOURS.toSeconds(1).toInt()
-            context.resources.getQuantityString(R.plurals.expiration_hours, hours, hours)
-        } else if (expirationTime < TimeUnit.DAYS.toSeconds(7)) {
-            val days = expirationTime / TimeUnit.DAYS.toSeconds(1).toInt()
-            context.resources.getQuantityString(R.plurals.expiration_days, days, days)
+        } else if (expirationTimeSecs < 1.minutes.inWholeSeconds) {
+            // If expiration time is expressed in seconds
+            val seconds = (expirationTimeSecs / 1.minutes.inWholeSeconds).toInt()
+            if (seconds == 1) {
+                Phrase.from(context, R.string.expirationSecond).put(SECONDS_KEY, seconds.toString()).format().toString()
+            }
+            else {
+                Phrase.from(context, R.string.expirationMinutes).put(SECONDS_KEY, seconds.toString()).format().toString()
+            }
+        } else if (expirationTimeSecs < 1.hours.inWholeSeconds) {
+            // If expiration time is expressed in minutes
+            val minutes = (expirationTimeSecs / 1.minutes.inWholeSeconds).toInt()
+            if (minutes == 1) {
+                Phrase.from(context, R.string.expirationMinute).put(MINUTES_KEY, minutes.toString()).format().toString()
+            }
+            else {
+                Phrase.from(context, R.string.expirationMinutes).put(MINUTES_KEY, minutes.toString()).format().toString()
+            }
+        } else if (expirationTimeSecs < 1.days.inWholeSeconds) {
+            // If expiration time is expressed in hours
+            val hours = (expirationTimeSecs / 1.hours.inWholeSeconds).toInt()
+            if (hours == 1) {
+                Phrase.from(context, R.string.expirationHour).put(HOURS_KEY, hours.toString()).format().toString()
+            }
+            else {
+                Phrase.from(context, R.string.expirationHours).put(HOURS_KEY, hours.toString()).format().toString()
+            }
+        } else if (expirationTimeSecs < TimeUnit.DAYS.toSeconds(7)) {
+            // If expiration time is expressed in days
+            val days = (expirationTimeSecs / 1.days.inWholeSeconds).toInt()
+            if (days == 1) {
+                Phrase.from(context, R.string.expirationDay).put(DAYS_KEY, days.toString()).format().toString()
+            }
+            else {
+                Phrase.from(context, R.string.expirationHours).put(DAYS_KEY, days.toString()).format().toString()
+            }
         } else {
-            val weeks = expirationTime / TimeUnit.DAYS.toSeconds(7).toInt()
-            context.resources.getQuantityString(R.plurals.expiration_weeks, weeks, weeks)
+            // If expiration time is expressed in weeks
+            val weeks = expirationTimeSecs / TimeUnit.DAYS.toSeconds(7).toInt()
+            if (weeks == 1) {
+                Phrase.from(context, R.string.expirationWeek).put(WEEKS_KEY, weeks.toString()).format().toString()
+            }
+            else {
+                Phrase.from(context, R.string.expirationWeeks).put(WEEKS_KEY, weeks.toString()).format().toString()
+            }
+
         }
     }
 
-    fun getExpirationAbbreviatedDisplayValue(context: Context, expirationTime: Long): String {
-        return if (expirationTime < TimeUnit.MINUTES.toSeconds(1)) {
-            Phrase.from(context, R.string.expirationSecondsAbbreviated).put(SECONDS_KEY, expirationTime.toString()).format().toString()
-        } else if (expirationTime < TimeUnit.HOURS.toSeconds(1)) {
-            val minutes = expirationTime / TimeUnit.MINUTES.toSeconds(1)
-            context.resources.getString(R.string.expiration_minutes_abbreviated, minutes)
-        } else if (expirationTime < TimeUnit.DAYS.toSeconds(1)) {
-            val hours = expirationTime / TimeUnit.HOURS.toSeconds(1)
-            context.resources.getString(R.string.expiration_hours_abbreviated, hours)
-        } else if (expirationTime < TimeUnit.DAYS.toSeconds(7)) {
-            val days = expirationTime / TimeUnit.DAYS.toSeconds(1)
-            context.resources.getString(R.string.expiration_days_abbreviated, days)
+    fun getExpirationAbbreviatedDisplayValue(context: Context, expirationTimeSecs: Long) =
+        if (expirationTimeSecs < TimeUnit.MINUTES.toSeconds(1)) {
+            Phrase.from(context, R.string.expirationSecondsAbbreviated).put(SECONDS_KEY, expirationTimeSecs.toString()).format().toString()
+        } else if (expirationTimeSecs < TimeUnit.HOURS.toSeconds(1)) {
+            val minutes = expirationTimeSecs / TimeUnit.MINUTES.toSeconds(1)
+            Phrase.from(context, R.string.expirationMinutesAbbreviated).put(MINUTES_KEY, minutes.toString()).format().toString()
+        } else if (expirationTimeSecs < TimeUnit.DAYS.toSeconds(1)) {
+            val hours = expirationTimeSecs / TimeUnit.HOURS.toSeconds(1)
+            Phrase.from(context, R.string.expirationHoursAbbreviated).put(HOURS_KEY, hours.toString()).format().toString()
+        } else if (expirationTimeSecs < TimeUnit.DAYS.toSeconds(7)) {
+            val days = expirationTimeSecs / TimeUnit.DAYS.toSeconds(1)
+            Phrase.from(context, R.string.expirationDaysAbbreviated).put(DAYS_KEY, days.toString()).format().toString()
         } else {
-            val weeks = expirationTime / TimeUnit.DAYS.toSeconds(7)
-            context.resources.getString(R.string.expiration_weeks_abbreviated, weeks)
+            val weeks = expirationTimeSecs / TimeUnit.DAYS.toSeconds(7)
+            Phrase.from(context, R.string.expirationWeeksAbbreviated).put(WEEKS_KEY, weeks.toString()).format().toString()
         }
-    }
+
 }
