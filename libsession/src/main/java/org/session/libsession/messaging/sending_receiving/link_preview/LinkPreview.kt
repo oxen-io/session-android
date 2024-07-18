3f2c288a5d9f91b4b5685f5bb67f2fe2f9ca7822
bemusementpark <bemusementpark>
2024-07-18 20:44:59 +0930

    Simplify LinkPreview

--- libsession/src/main/java/org/session/libsession/messaging/sending_receiving/link_preview/LinkPreview.kt
+++ libsession/src/main/java/org/session/libsession/messaging/sending_receiving/link_preview/LinkPreview.kt
@@ -6,63 +6,24 @@ import org.session.libsession.messaging.sending_receiving.attachments.Attachment
 import org.session.libsession.messaging.sending_receiving.attachments.AttachmentId
 import org.session.libsession.messaging.sending_receiving.attachments.DatabaseAttachment
 import org.session.libsignal.utilities.JsonUtil
-import org.session.libsignal.utilities.guava.Optional
 import java.io.IOException
-import java.util.Objects
 
-class LinkPreview {
-    @JsonProperty
-    val url: String
-
-    @JsonProperty
-    val title: String
-
-    @JsonProperty
-    val attachmentId: AttachmentId?
-
-    @JsonIgnore
-    var thumbnail: Optional<Attachment>
-
-    constructor(url: String, title: String, thumbnail: DatabaseAttachment) {
-        this.url = url
-        this.title = title
-        this.thumbnail = Optional.of(thumbnail)
-        this.attachmentId = thumbnail.attachmentId
-    }
-
-    constructor(url: String, title: String, thumbnail: Optional<Attachment>) {
-        this.url = url
-        this.title = title
-        this.thumbnail = thumbnail
-        this.attachmentId = null
-    }
-
+data class LinkPreview(
+    @JsonProperty val url: String,
+    @JsonProperty val title: String,
+    @JsonProperty val attachmentId: AttachmentId?,
+    @JsonIgnore var thumbnail: Attachment?
+) {
+    constructor(url: String, title: String, thumbnail: DatabaseAttachment): this(url, title, thumbnail.attachmentId, thumbnail)
+    constructor(url: String, title: String, thumbnail: Attachment?): this(url, title, null, thumbnail)
     constructor(
         @JsonProperty("url") url: String,
         @JsonProperty("title") title: String,
         @JsonProperty("attachmentId") attachmentId: AttachmentId?
-    ) {
-        this.url = url
-        this.title = title
-        this.attachmentId = attachmentId
-        this.thumbnail = Optional.absent()
-    }
+    ): this(url, title, attachmentId, null)
 
     @Throws(IOException::class)
-    fun serialize(): String {
-        return JsonUtil.toJsonThrows(this)
-    }
-
-    override fun equals(o: Any?): Boolean {
-        if (this === o) return true
-        if (o == null || javaClass != o.javaClass) return false
-        val that = o as LinkPreview
-        return url == that.url && title == that.title && attachmentId == that.attachmentId && thumbnail == that.thumbnail
-    }
-
-    override fun hashCode(): Int {
-        return Objects.hash(url, title, attachmentId, thumbnail)
-    }
+    fun serialize(): String = JsonUtil.toJsonThrows(this)
 
     companion object {
         @Throws(IOException::class)
