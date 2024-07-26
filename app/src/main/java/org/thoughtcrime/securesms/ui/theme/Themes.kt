4620abfa8fed42a0e0ee8c28f70585050dd713a4
ThomasSession <thomas.r@getsession.org>
2024-07-26 16:37:01 +1000

    Making sure the theme is updated when we switch the system settings

--- app/src/main/java/org/thoughtcrime/securesms/ui/theme/Themes.kt
+++ app/src/main/java/org/thoughtcrime/securesms/ui/theme/Themes.kt
@@ -1,6 +1,7 @@
 package org.thoughtcrime.securesms.ui.theme
 
 import androidx.compose.foundation.background
+import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.selection.LocalTextSelectionColors
@@ -20,7 +21,12 @@ import org.session.libsession.utilities.AppTextSecurePreferences
 val LocalColors = compositionLocalOf <ThemeColors> { ClassicDark() }
 val LocalType = compositionLocalOf { sessionTypography }
 
+// Once the app is entirely in Compose we won't need these two properties since we'll have
+// a single root composable which will get the theme as a state and update the app accordingly
+// without having to keep track of these flags, which are sure to cause syncing issues...
+// We can remove them once we have a unique Composable root or once we use the sharedPrefs as flows (PR in review)
 var selectedTheme: ThemeColors? = null
+var systemInDark: Boolean? = null
 
 /**
  * Apply a Material2 compose theme based on user selections in SharedPreferences.
@@ -29,11 +35,14 @@ var selectedTheme: ThemeColors? = null
 fun SessionMaterialTheme(
     content: @Composable () -> Unit
 ) {
+    val context = LocalContext.current
+    val preferences = AppTextSecurePreferences(context)
+
     // set the theme data if it hasn't been done yet
-    if(selectedTheme == null) {
+    // or if the user has changed their light/dark system settings in the background
+    if(selectedTheme == null ||
+        (systemInDark != isSystemInDarkTheme()) && preferences.getFollowSystemSettings()) {
         // Some values can be set from the preferences, and if not should fallback to a default value
-        val context = LocalContext.current
-        val preferences = AppTextSecurePreferences(context)
         selectedTheme = preferences.getComposeTheme()
     }
 
