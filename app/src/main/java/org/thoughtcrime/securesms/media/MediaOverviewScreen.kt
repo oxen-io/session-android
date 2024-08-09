ab4be7b085749fe619725e6cbdc1886ea345b055
fanchao <git@fanchao.dev>
2024-08-09 16:58:51 +1000

    Tidy up media page

--- app/src/main/java/org/thoughtcrime/securesms/media/MediaOverviewScreen.kt
+++ app/src/main/java/org/thoughtcrime/securesms/media/MediaOverviewScreen.kt
@@ -3,20 +3,11 @@ package org.thoughtcrime.securesms.media
 import androidx.activity.compose.BackHandler
 import androidx.compose.animation.Crossfade
 import androidx.compose.foundation.ExperimentalFoundationApi
-import androidx.compose.foundation.Image
-import androidx.compose.foundation.background
-import androidx.compose.foundation.clickable
-import androidx.compose.foundation.combinedClickable
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.pager.HorizontalPager
 import androidx.compose.foundation.pager.rememberPagerState
 import androidx.compose.material.icons.Icons
@@ -29,28 +20,27 @@ import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
 import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.material3.TopAppBarScrollBehavior
 import androidx.compose.material3.rememberTopAppBarState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.input.nestedscroll.NestedScrollConnection
 import androidx.compose.ui.input.nestedscroll.nestedScroll
-import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextAlign
-import androidx.compose.ui.unit.dp
-import com.bumptech.glide.integration.compose.CrossFade
-import com.bumptech.glide.integration.compose.ExperimentalGlideComposeApi
-import com.bumptech.glide.integration.compose.GlideImage
-import com.bumptech.glide.integration.compose.Transition
 import network.loki.messenger.R
+import org.thoughtcrime.securesms.ui.AlertDialog
+import org.thoughtcrime.securesms.ui.DialogButtonModel
+import org.thoughtcrime.securesms.ui.GetString
 import org.thoughtcrime.securesms.ui.theme.LocalColors
 import org.thoughtcrime.securesms.ui.theme.LocalType
-import kotlin.math.ceil
 
 @OptIn(ExperimentalFoundationApi::class, ExperimentalMaterial3Api::class)
 @Composable
@@ -60,15 +50,17 @@ fun MediaOverviewScreen(
 ) {
     val selected by viewModel.selectedItemIDs.collectAsState()
     val selectionMode by viewModel.inSelectionMode.collectAsState()
-
     val topAppBarState = rememberTopAppBarState()
+    var showingDeleteConfirmation by remember { mutableStateOf(false) }
 
+    // In selection mode, the app bar should not be scrollable and should be pinned
     val appBarScrollBehavior = if (selectionMode) {
         TopAppBarDefaults.pinnedScrollBehavior(topAppBarState, canScroll = { false })
     } else {
         TopAppBarDefaults.enterAlwaysScrollBehavior(topAppBarState)
     }
 
+    // Reset the top app bar offset (so that it shows up) when entering selection mode
     LaunchedEffect(selectionMode) {
         if (selectionMode) {
             topAppBarState.heightOffset = 0f
@@ -91,51 +83,16 @@ fun MediaOverviewScreen(
     Scaffold(
         modifier = Modifier.nestedScroll(appBarScrollBehavior.nestedScrollConnection),
         topBar = {
-            TopAppBar(
-                title = {
-                    if (selectionMode) {
-                        Text(selected.size.toString())
-                    } else {
-                        Text(viewModel.title.collectAsState().value)
-                    }
-                },
-                navigationIcon = {
-                    IconButton(viewModel::onBackClicked) {
-                        Icon(
-                            Icons.AutoMirrored.Default.ArrowBack,
-                            contentDescription = stringResource(R.string.AccessibilityId_done),
-                            tint = LocalColors.current.text
-                        )
-                    }
-                },
-                scrollBehavior = appBarScrollBehavior,
-                actions = {
-                    if (selectionMode) {
-                        IconButton(onClick = viewModel::onSaveClicked) {
-                            Icon(
-                                painterResource(R.drawable.ic_baseline_save_24),
-                                contentDescription = stringResource(R.string.save),
-                                tint = LocalColors.current.text,
-                            )
-                        }
-
-                        IconButton(onClick = viewModel::onDeleteClicked) {
-                            Icon(
-                                painterResource(R.drawable.ic_baseline_delete_24),
-                                contentDescription = stringResource(R.string.delete),
-                                tint = LocalColors.current.text,
-                            )
-                        }
-
-                        IconButton(onClick = viewModel::onSelectAllClicked) {
-                            Icon(
-                                painterResource(R.drawable.ic_baseline_select_all_24),
-                                contentDescription = stringResource(R.string.MediaOverviewActivity_Select_all),
-                                tint = LocalColors.current.text,
-                            )
-                        }
-                    }
-                })
+            MediaOverviewTopAppBar(
+                selectionMode = selectionMode,
+                selected = selected,
+                title = viewModel.title.collectAsState().value,
+                onBackClicked = viewModel::onBackClicked,
+                onSaveClicked = viewModel::onSaveClicked,
+                onDeleteClicked = { showingDeleteConfirmation = true },
+                onSelectAllClicked = viewModel::onSelectAllClicked,
+                appBarScrollBehavior = appBarScrollBehavior
+            )
         }
     ) { paddings ->
         Column(
@@ -145,8 +102,15 @@ fun MediaOverviewScreen(
         ) {
             val pagerState = rememberPagerState(pageCount = { MediaOverviewTab.entries.size })
             val selectedTab by viewModel.selectedTab.collectAsState()
+
+            // Apply "selectedTab" view model state to pager
             LaunchedEffect(selectedTab) {
                 pagerState.animateScrollToPage(selectedTab.ordinal)
+            }
+
+            // Apply "selectedTab" pager state to view model
+            LaunchedEffect(pagerState.currentPage) {
+                viewModel.onTabItemClicked(MediaOverviewTab.entries[pagerState.currentPage])
             }
 
             Row(modifier = Modifier.fillMaxWidth()) {
@@ -195,6 +159,89 @@ fun MediaOverviewScreen(
             }
         }
     }
+
+    if (showingDeleteConfirmation) {
+        val context = LocalContext.current
+
+        AlertDialog(
+            onDismissRequest = { showingDeleteConfirmation = false },
+            title = context.resources.getQuantityString(
+                R.plurals.ConversationFragment_delete_selected_messages, selected.size
+            ),
+            text = context.resources.getQuantityString(
+                R.plurals.ConversationFragment_this_will_permanently_delete_all_n_selected_messages,
+                selected.size,
+                selected.size
+            ),
+            buttons = listOf(
+                DialogButtonModel(GetString(R.string.delete), onClick = viewModel::onDeleteClicked),
+                DialogButtonModel(GetString(android.R.string.cancel), dismissOnClick = true)
+            )
+        )
+    }
+}
+
+@Composable
+@OptIn(ExperimentalMaterial3Api::class)
+private fun MediaOverviewTopAppBar(
+    selectionMode: Boolean,
+    selected: Set<Long>,
+    title: String,
+    onBackClicked: () -> Unit,
+    onSaveClicked: () -> Unit,
+    onDeleteClicked: () -> Unit,
+    onSelectAllClicked: () -> Unit,
+    appBarScrollBehavior: TopAppBarScrollBehavior
+) {
+    TopAppBar(
+        title = {
+            if (selectionMode) {
+                Text(selected.size.toString())
+            } else {
+                Text(title)
+            }
+        },
+        navigationIcon = {
+            IconButton(onBackClicked) {
+                Icon(
+                    Icons.AutoMirrored.Default.ArrowBack,
+                    contentDescription = stringResource(R.string.AccessibilityId_done),
+                    tint = LocalColors.current.text
+                )
+            }
+        },
+        scrollBehavior = appBarScrollBehavior,
+        actions = {
+            Crossfade(selectionMode, label = "Action icons animation") { mode ->
+                if (mode) {
+                    Row {
+                        IconButton(onClick = onSaveClicked) {
+                            Icon(
+                                painterResource(R.drawable.ic_baseline_save_24),
+                                contentDescription = stringResource(R.string.save),
+                                tint = LocalColors.current.text,
+                            )
+                        }
+
+                        IconButton(onClick = onDeleteClicked) {
+                            Icon(
+                                painterResource(R.drawable.ic_baseline_delete_24),
+                                contentDescription = stringResource(R.string.delete),
+                                tint = LocalColors.current.text,
+                            )
+                        }
+
+                        IconButton(onClick = onSelectAllClicked) {
+                            Icon(
+                                painterResource(R.drawable.ic_baseline_select_all_24),
+                                contentDescription = stringResource(R.string.MediaOverviewActivity_Select_all),
+                                tint = LocalColors.current.text,
+                            )
+                        }
+                    }
+                }
+            }
+        })
 }
 
 private val MediaOverviewTab.titleResId: Int
@@ -208,122 +255,3 @@ private fun DocumentsPage() {
     Text("Documents Page")
 }
 
-@OptIn(ExperimentalFoundationApi::class)
-@Composable
-private fun MediaPage(
-    nestedScrollConnection: NestedScrollConnection,
-    content: TabContent?,
-    selectedItemIDs: Set<Long>,
-    onItemClicked: (Long) -> Unit,
-    onItemLongClicked: ((Long) -> Unit)?,
-) {
-    val columnCount = 3
-
-    when {
-        content == null -> {
-            // Loading state
-        }
-
-        content.isEmpty() -> {
-            // Empty state
-        }
-
-        else -> {
-            LazyColumn(
-                modifier = Modifier
-                    .nestedScroll(nestedScrollConnection)
-                    .padding(2.dp),
-                verticalArrangement = Arrangement.spacedBy(2.dp)
-            ) {
-                for ((header, thumbnails) in content) {
-                    stickyHeader {
-                        Text(
-                            modifier = Modifier
-                                .background(LocalColors.current.background)
-                                .fillMaxWidth()
-                                .padding(horizontal = 16.dp, vertical = 8.dp),
-                            text = header,
-                            style = LocalType.current.base,
-                            color = LocalColors.current.text
-                        )
-                    }
-
-                    val numRows = ceil(thumbnails.size / columnCount.toFloat()).toInt()
-
-                    // Row of thumbnails
-                    items(numRows) { rowIndex ->
-                        ThumbnailRow(
-                            columnCount = columnCount,
-                            thumbnails = thumbnails,
-                            rowIndex = rowIndex,
-                            onItemClicked = onItemClicked,
-                            onItemLongClicked = onItemLongClicked,
-                            selectedItemIDs = selectedItemIDs
-                        )
-                    }
-                }
-            }
-        }
-    }
-}
-
-@Composable
-@OptIn(ExperimentalGlideComposeApi::class, ExperimentalFoundationApi::class)
-private fun ThumbnailRow(
-    columnCount: Int,
-    thumbnails: List<MediaOverviewItem>,
-    rowIndex: Int,
-    onItemClicked: (Long) -> Unit,
-    onItemLongClicked: ((Long) -> Unit)?,
-    selectedItemIDs: Set<Long>
-) {
-    Row(horizontalArrangement = Arrangement.spacedBy(2.dp)) {
-        repeat(columnCount) { columnIndex ->
-            val item = thumbnails.getOrNull(rowIndex * columnCount + columnIndex)
-            if (item != null) {
-                Box(
-                    modifier = Modifier
-                        .weight(1f)
-                        .aspectRatio(1f)
-                        .let {
-                            if (onItemLongClicked != null) {
-                                it.combinedClickable(
-                                    onClick = { onItemClicked(item.id) },
-                                    onLongClick = { onItemLongClicked(item.id) }
-                                )
-                            } else {
-                                it.clickable { onItemClicked(item.id) }
-                            }
-                        }
-                ) {
-                    GlideImage(
-                        item.slide.thumbnailUri,
-                        modifier = Modifier.fillMaxSize(),
-                        contentScale = ContentScale.Crop,
-                        contentDescription = null,
-                        transition = CrossFade,
-                    )
-
-                    Crossfade(
-                        modifier = Modifier.fillMaxSize(),
-                        targetState = item.id in selectedItemIDs,
-                        label = "Showing selected state"
-                    ) { selected ->
-                        if (selected) {
-                            Image(
-                                modifier = Modifier
-                                    .fillMaxSize()
-                                    .background(Color.Black.copy(alpha = 0.4f)),
-                                contentScale = ContentScale.Inside,
-                                painter = painterResource(R.drawable.ic_check_white_48dp),
-                                contentDescription = stringResource(R.string.AccessibilityId_select),
-                            )
-                        }
-                    }
-                }
-            } else {
-                Spacer(modifier = Modifier.weight(1f))
-            }
-        }
-    }
-}
