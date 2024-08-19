5e02e1ef5c04056761409c97ba90efc5b447bb6c
alansley <aclansley@gmail.com>
2024-08-20 09:39:16 +1000

    Added 'NonTranslatableStringConstants' file

---
+++ libsession/src/main/java/org/session/libsession/utilities/NonTranslatableStringConstants.kt
@@ -0,0 +1,13 @@
+package org.session.libsession.utilities
+
+// Non-translatable strings for use with the UI
+object StringSubstitutionConstants {
+    const val APP_NAME               = "Session"
+    const val ARBISCAN               = "Arbiscan"
+    const val ARBITRUM               = "Arbitrum"
+    const val ETHEREUM               = "Ethereum"
+    const val NETWORK_NAME           = "Session Network"
+    const val TOKEN_NAME_LONG        = "Session Token"
+    const val TOKEN_NAME_LONG_PLURAL = "Session Tokens"
+    const val TOKEN_NAME_SHORT       = "SENT"
+}
